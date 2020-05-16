1b8f3336c66f1644d50ba3f5500971c015bf03e5
Luis Garizabalo <luis.garizabalo@eltransporte.org>
2020-05-16 10:55:45 -0500

    [lucho] Se crea el archivo serve.js y se crea el Procfile

---
+++ server.js
@@ -0,0 +1,24 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// // Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/frontend-piccolino-angular'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/frontend-piccolino-angular/index.html'));
+});
+
+// // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+// const express = require('express');
+// const app = express();
+
+// // Run the app by serving the static files in the dist directory
+// app.use(express.static(__dirname + '/dist'));
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);

3ada4bc59c2e04e566574e295c2aab4681323a64
lgarizabalo <lgarizabalo@serempre.com>
2021-07-15 09:03:23 -0500

    [lgarizabalo] Se ajusta la ruta de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
+    // production: true,
+    // UrlApi: 'https://legasoft.sytes.net/Piccolino/api',
+    // ServerUrl: 'https://legasoft.sytes.net/Piccolino/'
     production: true,
-    UrlApi: 'https://legasoft.sytes.net/Piccolino/api',
-    ServerUrl: 'https://legasoft.sytes.net/Piccolino/'
-
+    UrlApi: 'https://fundacionpiccolino.com/api',
+    ServerUrl: 'https://fundacionpiccolino.com/'
 };
